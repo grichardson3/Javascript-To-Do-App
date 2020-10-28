26e39d50cbafb493e71ebc4fd1e321fc207b31f4
Gareth Richardson <grichardson@live.ca>
2020-10-28 12:24:32 -0400

    second commit

--- js/todo-app.js
+++ js/todo-app.js
@@ -1,74 +1,8 @@
-let toDo = [
-  {
-    id: 0,
-    title: "groceries",
-    completed: false,
-  },
-  {
-    id: 1,
-    title: "laundry",
-    completed: true,
-  },
-  {
-    id: 2,
-    title: "cook dinner",
-    completed: true,
-  },
-];
-
-// Filters to-do items with search
+const toDo = getSavedToDos();
 
 const filters = {
   searchText: '',
   hideCompleted: false
-}
-
-const renderToDos = (toDo, filters) => {
-
-  const filteredToDos = toDo.filter((todo) => {
-    const searchTextMatch = todo.title.toLowerCase().includes(filters.searchText.toLowerCase());
-    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-
-    return searchTextMatch && hideCompletedMatch;
-  });
-
-  if (!filteredToDos.length) {
-    document.querySelector('#toDos').innerHTML = '<p><i style="color: #666;">no results found...</i></p>';
-  } else {
-    document.querySelector('#toDos').innerHTML = '';
-  }
-
-  filteredToDos.forEach((todo) => {
-
-    const toDoElement = document.createElement('p');
-    toDoElement.setAttribute('class', 'paragraph');
-
-    /*const toDoDelete = document.createElement('a');
-    toDoDelete.setAttribute('href', '#');
-    toDoDelete.setAttribute('class', 'deleteItem');
-    toDoDelete.setAttribute('id', todo.id);
-    toDoDelete.textContent = "Delete Item";*/
-
-    if (!todo.completed) {
-      toDoElement.textContent = todo.title + " - " + "incomplete";
-      document.querySelector('#toDos').appendChild(toDoElement);
-      // document.querySelector('#toDos').appendChild(toDoDelete);
-    } else {
-      toDoElement.textContent = todo.title + " - " + "completed";
-      document.querySelector('#toDos').appendChild(toDoElement);
-      // document.querySelector('#toDos').appendChild(toDoDelete);
-    }
-  });
-
-  const incompleteToDos = toDo.filter((todo) => {
-    return !todo.completed;
-  });
-
-  if (document.querySelector("h3")) {
-    const changeSummary = document.querySelector("h3");
-    if (incompleteToDos.length > 1 || incompleteToDos.length == 0) {s = "s"} else {s = ""}
-    changeSummary.textContent = `You have ${incompleteToDos.length} incomplete todo${s} left`;
-  }
 }
 
 renderToDos(toDo, filters);
@@ -79,49 +13,22 @@ document.querySelector('#searchToDo').addEventListener('input', (e) => {
 });
 
 document.querySelector("#toDoForm").addEventListener('submit', (e) => {
-
   e.preventDefault();
-
   if (!e.target.elements.toDoInput.value == "") {
     toDo.push({
-      id: toDo.length,
+      id: uuidv4(),
       title: e.target.elements.toDoInput.value,
       completed: false
     });
   }
-
+  saveToDos(toDo);
   renderToDos(toDo, filters);
   e.target.elements.toDoInput.value = "";
 });
 
-document.querySelector('#hideCompleted').addEventListener("change", function (e) {
+document.querySelector('#hideCompleted').addEventListener("change", (e) => {
   filters.hideCompleted = e.target.checked;
   renderToDos(toDo, filters);
 });
 
-document.querySelectorAll('.deleteItem').forEach(element => {
-  element.addEventListener('click', (e) => {
-    // If statement that complares individual toDo.id to id attached on delete item button
-    // This should be a forEach function
-    /*console.log(e.target.id);
-    if (toDo.id == e.target.id) {
-      toDo.pop();
-    }*/
-  });
-  renderToDos(toDo, filters);
-});
-
-/*****/
-
-// Return and render incomplete to-do items
-
-const incompleteToDos = toDo.filter((todo) => {
-  return !todo.completed;
-});
-
-const summary = document.createElement("h3");
-if (incompleteToDos.length > 1 || incompleteToDos.length == 0) {s = "s"} else {s = ""}
-summary.textContent = `You have ${incompleteToDos.length} incomplete todo${s} left`;
-document.querySelector("body").appendChild(summary);
-
-/*****/
+renderDOMSummary();
---
+++ js/todo-functions.js
@@ -0,0 +1,120 @@
+const getSavedToDos = () => {
+    const toDoJSON = localStorage.getItem('toDo');
+    if (toDoJSON !== null) {
+        return JSON.parse(toDoJSON);
+    } else {
+        return [];
+    }
+}
+
+const saveToDos = (toDo) => {
+    localStorage.setItem('toDo', JSON.stringify(toDo));
+}
+
+const removeToDo = (id) => {
+    const toDoIndex = toDo.findIndex((todo) => {
+        return todo.id === id;
+    });
+
+    if (toDoIndex > -1) {
+        toDo.splice(toDoIndex, 1);
+    }
+}
+
+const renderToDos = (toDo, filters) => {
+
+    const filteredToDos = toDo.filter((todo) => {
+        const searchTextMatch = todo.title.toLowerCase().includes(filters.searchText.toLowerCase());
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+        return searchTextMatch && hideCompletedMatch;
+    });
+
+    if (!filteredToDos.length) {
+        document.querySelector('#toDos').innerHTML = '<p><i style="color: #666;">no results found...</i></p>';
+    } else {
+        document.querySelector('#toDos').innerHTML = '';
+    }
+
+    filteredToDos.forEach((todo) => {
+
+        const toDoElement = document.createElement('div');
+        const checkbox = document.createElement('input');
+        const toDoText = document.createElement('span');
+        const deleteButton = document.createElement('button');
+        checkbox.setAttribute('type', 'checkbox');
+        deleteButton.textContent = "Delete";
+
+        checkbox.addEventListener('change', (e) => {
+            if (e.target.checked == true) {
+                todo.completed = true;
+                saveToDos(toDo);
+                renderToDos(toDo, filters);
+            } else {
+                todo.completed = false;
+                saveToDos(toDo);
+                renderToDos(toDo, filters);
+            }
+        });
+
+        deleteButton.addEventListener('click', (e) => {
+            removeToDo(todo.id);
+            saveToDos(toDo);
+            renderToDos(toDo, filters);
+        });
+
+        if (todo.title.length > 0) {
+            toDoText.textContent = todo.title;
+        } else {
+            toDoText.textContent = "Unnamed to-do";
+        }
+
+        if (!todo.completed) {
+            // toDoText.textContent = todo.title + " - incomplete";
+            toDoText.textContent = todo.title;
+            document.querySelector('#toDos').appendChild(toDoElement);
+            toDoElement.appendChild(checkbox);
+            toDoElement.appendChild(deleteButton);
+            toDoElement.appendChild(toDoText);
+            checkbox.checked = false;
+        } else {
+            // toDoText.textContent = todo.title + " - completed";
+            toDoText.textContent = todo.title;
+            document.querySelector('#toDos').appendChild(toDoElement);
+            toDoElement.appendChild(checkbox);
+            toDoElement.appendChild(deleteButton);
+            toDoElement.appendChild(toDoText);
+            checkbox.checked = true;
+        }
+    });
+
+    const incompleteToDos = toDo.filter((todo) => {
+        return !todo.completed;
+    });
+
+    if (document.querySelector("h3")) {
+        const changeSummary = document.querySelector("h3");
+        if (incompleteToDos.length > 1 || incompleteToDos.length == 0) {s = "s"} else {s = ""}
+        changeSummary.textContent = `You have ${incompleteToDos.length} incomplete todo${s} left`;
+    }
+}
+
+const renderToDoDOM = (todo) => {
+    const toDoElement = document.createElement('div');
+    if (todo.title.length > 0) {
+        toDoElement.textContent = todo.title;
+    } else {
+        toDoElement.textContent = "Unnamed to-do";
+    }
+    return toDoElement;
+}
+
+const renderDOMSummary = () => {
+    const incompleteToDos = toDo.filter((todo) => {
+        return !todo.completed;
+    });
+    const summary = document.createElement("h3");
+    if (incompleteToDos.length > 1 || incompleteToDos.length == 0) {s = "s"} else {s = ""}
+    summary.textContent = `You have ${incompleteToDos.length} incomplete todo${s} left`;
+    document.querySelector("body").appendChild(summary);
+}
